ff3ddc94b68d0608cfdb5eaa860e8e78c36e9442
Cellowes <tomasshelby779@gmail.com>
2023-12-20 17:04:06 +0500

    config tugadi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+     Colors: {
+        'brandRed' : '#DD3333',
+        'darkBlue' : '#283648',
+        'mainblack' : '#171717',
+        'mainwhite' : '#FFFFFF',
+        'secondaryblack':'#272828',
+        'gray0' : '#697583',
+        'gray1': '#A4B0BE',
+        'gray2' : '#E1E3E6',
+        'gray3':'#F4F4F4',
+        'bgcolor' : '#FAFAFA'
+      },
+    },
   },
   plugins: [],
 }
